9aca6f4673b6323e7179cf9048904b1cb401e011
Pawel Stepniewski <pstepniewski@sylogic.pl>
2018-05-12 13:00:41 +0200

    Angular Tutorial: Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +12,17 @@ import { Hero } from '../hero'
 
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
